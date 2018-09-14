2fcb36ece32e52bd83adc330e1820a295c1ea87c
Portree Kid <keith.paterson@gmx.de>
2018-09-14 14:55:39 +0200

    Test

---
+++ src/test/java/org/flightgear/terramaster/TestTilename.java
@@ -0,0 +1,31 @@
+package org.flightgear.terramaster;
+
+import static org.junit.Assert.assertEquals;
+
+import org.flightgear.terramaster.TileName;
+import org.junit.Test;
+
+public class TestTilename {
+
+  @Test
+  public void test1() {
+    java.awt.geom.Point2D.Double d = new java.awt.geom.Point2D.Double(7.5, 50);
+    int result = TileName.getTileIndex(d);
+    assertEquals(3072770, result);
+  }
+
+  @Test
+  public void test2() {
+    java.awt.geom.Point2D.Double d = new java.awt.geom.Point2D.Double(-7.5, -50);
+    int result = TileName.getTileIndex(d);
+    assertEquals(2820610, result);
+  }
+
+  @Test
+  public void test3() {
+    java.awt.geom.Point2D.Double d = new java.awt.geom.Point2D.Double(-5.74110808, 57.2786546);
+    int result = TileName.getTileIndex(d);
+    assertEquals(2860241, result);
+  }
+
+}
