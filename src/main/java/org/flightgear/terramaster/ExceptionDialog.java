a1e17f8ba0aa1fb72c01c8e84a5cd83534dc4b01
Portree Kid <keith.paterson@gmx.de>
2018-09-24 07:52:39 +0200

    Removed main

--- src/main/java/org/flightgear/terramaster/ExceptionDialog.java
+++ src/main/java/org/flightgear/terramaster/ExceptionDialog.java
@@ -28,19 +28,6 @@ public class ExceptionDialog extends JDialog {
   private final JPanel contentPanel = new JPanel();
 
   /**
-   * Launch the application.
-   */
-  public static void main(String[] args) {
-    try {
-      ExceptionDialog dialog = new ExceptionDialog(null);
-      dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-      dialog.setVisible(true);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  /**
    * Create the dialog.
    * 
    * @param downloadStats
